bde5a06b0a58eb768341950b51f2a45ccbdc6c21
athif05 <athif.hussain05@gmail.com>
2025-11-14 01:39:26 +0530

    pull from server

--- config/services.php
+++ config/services.php
@@ -19,6 +19,7 @@ return [
         'ad_account_id' => env('FACEBOOK_AD_ACCOUNT_ID'),
         'access_token' => env('FACEBOOK_ACCESS_TOKEN'),
         'app_id' => env('FACEBOOK_APP_ID'),
+        'client_id' => env('FACEBOOK_APP_ID'),
         'app_secret' => env('FACEBOOK_APP_SECRET'),
         'redirect' => env('FACEBOOK_REDIRECT_URI'),
         'scopes' => env('FACEBOOK_SCOPES'),
